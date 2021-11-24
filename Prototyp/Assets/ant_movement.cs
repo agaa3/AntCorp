62a041949ba4d17059b16e3bec9d1e4110be195b
Poooowero <bartek2002@interia.pl>
2021-11-24 17:16:26 +0100

    Wersja_na_deadline

--- Prototyp/Assets/ant_movement.cs
+++ Prototyp/Assets/ant_movement.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class ant_movement : MonoBehaviour
 {
-    public float MovementSpeed = 1;
-    public float JumpForce = 1;
+    [Header("Movement")]
+    public float MovementSpeed = 10;
+    public float JumpForce = 10;
+
+    private bool m_FacingRight = true;
+
 
     private Rigidbody2D PlayerAnt;
    private void Start()
@@ -14,12 +18,41 @@ public class ant_movement : MonoBehaviour
   
     private void Update()
     {
-        var movement = Input.GetAxis("Horizontal");             //poruszanie sie lewo/prawo
-        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+        Move();
+        Jump();
+        
 
-        if(Input.GetButtonDown("Jump")&& Mathf.Abs(PlayerAnt.velocity.y)<0.001f)       //skok
+        //Cursor.lockState = CursorLockMode.Locked;  //Placeholder na celownik
+      //  Cursor.visible = false;
+    }
+
+    private void Move()
+    {
+        
+        var movement = Input.GetAxisRaw("Horizontal");             //poruszanie sie lewo/prawo
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+        if(movement > 0 && !m_FacingRight)
         {
-            PlayerAnt.AddForce(new Vector2(0, JumpForce),ForceMode2D.Impulse);
+            Flip();
         }
+        else if(movement < 0 && m_FacingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(PlayerAnt.velocity.y) < 0.001f)       //skok
+        {
+            PlayerAnt.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void Flip()
+    {
+        m_FacingRight = !m_FacingRight;
+
+        transform.Rotate(0f, 180f, 0f);
     }
 }
