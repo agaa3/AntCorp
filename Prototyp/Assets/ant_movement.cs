12718505bba7fab0ea8ee302459d5379f757d0f0
Poooowero <bartek2002@interia.pl>
2021-12-07 20:51:28 +0100

    chodzenie po scianach i woda - prototyp

--- Prototyp/Assets/ant_movement.cs
+++ Prototyp/Assets/ant_movement.cs
@@ -1,49 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
 
 public class ant_movement : MonoBehaviour
 {
-    [Header("Movement")]
+    //  [Header("Movement")]
+    public Rigidbody2D PlayerAnt;
     public float MovementSpeed = 10;
     public float JumpForce = 10;
-
+    public float checkRadius;
     private bool m_FacingRight = true;
-
-
-    private Rigidbody2D PlayerAnt;
-   private void Start()
+    bool wallSliding;
+    public float wallSlidingSpeed;
+    public Transform frontCheck;
+    bool isTouchingFront;
+    private void Start()
     {
         PlayerAnt = GetComponent<Rigidbody2D>();
     }
 
-  
+
     private void Update()
     {
         Move();
         Jump();
-        
+        wallWalk();
+
 
         //Cursor.lockState = CursorLockMode.Locked;  //Placeholder na celownik
-      //  Cursor.visible = false;
+        //  Cursor.visible = false;
     }
 
     private void Move()
     {
-        
+
         var movement = Input.GetAxisRaw("Horizontal");             //poruszanie sie lewo/prawo
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
-        if(movement > 0 && !m_FacingRight)
+        if (movement > 0 && !m_FacingRight)
         {
             Flip();
         }
-        else if(movement < 0 && m_FacingRight)
+        else if (movement < 0 && m_FacingRight)
         {
             Flip();
         }
     }
 
-    private void Jump()
+    
+    private void wallWalk()
     {
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(PlayerAnt.velocity.y) < 0.001f)       //skok
+        isTouchingFront = Physics2D.OverlapCircle(frontCheck.position, checkRadius);
+       
+
+        if (isTouchingFront == true && (Input.GetKey(KeyCode.W)|| Input.GetKey(KeyCode.S)) /*&& (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))*/)
+        {
+            wallSliding = true;
+        }
+        else
+        {
+            wallSliding = false;
+        }
+
+        if(isTouchingFront)
+        {
+            Physics2D.gravity = new Vector2(0, 0f);
+        }
+         else
+        {
+            Physics2D.gravity = new Vector2(0, -9f);
+        }
+
+        if (wallSliding)
+        {
+            var wallMovement = Input.GetAxisRaw("Vertical");
+            transform.position += new Vector3(0, wallMovement, 0) * Time.deltaTime * MovementSpeed;
+            // PlayerAnt.velocity = new Vector2(PlayerAnt.velocity.x, Mathf.Clamp(PlayerAnt.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+       else
+        {
+          //  transform.position -=new Vector3(0, wallMovement, 0) * Time.deltaTime * MovementSpeed;
+        }
+        
+    }
+
+        private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(PlayerAnt.velocity.y) < 0.001f)
         {
             PlayerAnt.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
         }
@@ -52,7 +95,6 @@ public class ant_movement : MonoBehaviour
     private void Flip()
     {
         m_FacingRight = !m_FacingRight;
-
         transform.Rotate(0f, 180f, 0f);
     }
 }
