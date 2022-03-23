ab9c7776fa92dfad6de1c279bd549c1487d79c17
boniffacy <94139663+boniffacy@users.noreply.github.com>
2022-03-23 18:38:32 +0100

    Stick counter + smallAnt error
    Patyki sie zliczają.
    Mrówki nie można podnieść przez HELP.

--- Assets/FriendsManager.cs
+++ Assets/FriendsManager.cs
@@ -5,9 +5,11 @@ using UnityEngine.UI;
 
 public class FriendsManager : MonoBehaviour
 {
+    public RawImage friendsCount;
     public static FriendsManager instance;
-    public Text text;
-    int friendsScore;
+    //public Text text;
+    int friendsScore = 0;
+    public Texture[] friendsNumbers = new Texture[10];
 
     // Start is called before the first frame update
     void Start()
@@ -16,12 +18,21 @@ public class FriendsManager : MonoBehaviour
         {
             instance = this;
         }
-        
+        friendsCount.texture = friendsNumbers[friendsScore];
     }
 
     public void ChangeFriendsScore()
     {
-        friendsScore += 1;
-        text.text = "X"+friendsScore.ToString();
+        if(friendsScore < friendsNumbers.Length - 1)
+        {
+            friendsScore++;
+            //text.text = "friends: "+friendsScore.ToString();
+            friendsCount.texture = friendsNumbers[friendsScore];
+        }
+        else
+        {
+            friendsCount.texture = friendsNumbers[friendsNumbers.Length - 1];
+        }
+        
     }
 }
