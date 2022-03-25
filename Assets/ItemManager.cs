993860e80e581ec9339accff094e8f8423e9cf9d
boniffacy <94139663+boniffacy@users.noreply.github.com>
2022-03-25 22:56:05 +0100

    Obrazkowy licznik cukierków
    + prefab candy

--- Assets/ItemManager.cs
+++ Assets/ItemManager.cs
@@ -8,6 +8,8 @@ public class ItemManager : MonoBehaviour
     public static ItemManager instance;
     public Text text;
     int itemNumber = 5; //na razie, do testow 
+    public RawImage numberOfCandiesImage;
+    public Texture[] allNumbers = new Texture[10];
 
 
     // Start is called before the first frame update
@@ -18,12 +20,14 @@ public class ItemManager : MonoBehaviour
             instance = this;
         }
         text.text = "X" + itemNumber.ToString();
+        numberOfCandiesImage.texture = allNumbers[itemNumber];
     }
 
     public void ChangeItemNumber()
     {
         itemNumber += 1;
         text.text = "X" + itemNumber.ToString();
+        numberOfCandiesImage.texture = allNumbers[itemNumber];
     }
 
 }
