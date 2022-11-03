c3382f7feea5501072fb0ca24912d188cb3c2d79
Poooowero <bartek2002@interia.pl>
2022-11-03 12:56:28 +0100

    dzialaaaaaaaaaaaaaaa

--- Assets/ant_movement.cs
+++ Assets/ant_movement.cs
@@ -1,38 +1,9 @@
-// Przy budowaniu mapy nale¿y umiescic na ka¿dym rogu teleport , inacze findDestinationTeleport()/findStartTeleport() zwraca null i gierka sie wysypie
-//Sterowanie:
-//  E: wejœcie na œcianê
-//  R: wdrapanie sie na górê 
-//  F: oderwanie siê od œciany
-//TODO:
-//switch na pocz¹tku i koñcu animacji
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ant_movement : MonoBehaviour
 {
-    public class teleports_pair
-    {
-        public Transform getTeleport1()
-        {
-            return teleport1;
-        }
-        public Transform getTeleport2()
-        {
-            return teleport2;
-        }
-
-        public teleports_pair(Transform t1, Transform t2)
-        {
-            teleport1 = t1;
-            teleport2 = t2;
-        }
-
-        private Transform teleport1;
-        private Transform teleport2;
-    };
-
-
     public GameObject topAheadDetector;
     public GameObject downAheadDetector;
     public GameObject topBehindDetector;
@@ -60,10 +31,6 @@ public class ant_movement : MonoBehaviour
     private bool m_FacingRight = true;
     private bool couldAntMove = true;
 
-    public static int NUMBER_OF_TELEPORTS_PAIRS = 6;
-    public static float distanceFromStartingTeleport = 0.5f;
-    public static float distanceFromDestinationTeleport = 1.65f;
-    public List<teleports_pair> teleports = new List<teleports_pair>();
     float MOVEMENT;
 
 
@@ -81,9 +48,7 @@ public class ant_movement : MonoBehaviour
         boxCollider2d = transform.GetComponent<BoxCollider2D>();
         animator.SetInteger("wallClimbSide", 0);
         Physics2D.gravity = new Vector2(0, -9.81f);
-        setTeleportsInArray();
         PlayerAnt.gravityScale = 1;
-
     }
 
     private void Update()
@@ -122,27 +87,22 @@ public class ant_movement : MonoBehaviour
     {
             transform.position += new Vector3(MOVEMENT, 0, 0) * Time.deltaTime * MovementSpeed;
             
-            //doNotFallDownFromPlatform(movement);
-            //flippingWhenAntIsOnTheFloor(movement);
+            doNotFallDownFromPlatform();
+            flippingWhenAntIsOnTheFloor();
             rightClimbOnWall();
-            //leftClimbOnWall();
-            //rightGoDown();
     }
 
     public void antWalk()
     {
+        antCanMove();
+        turnOnGravity();
         animator.SetInteger("wallClimbSide", 6969);
     }
 
     private void climb()
     {
-        //goDownFromRightWallToFloor();
         upToDownWallMoveRight();
-        //upToDownWallMoveLeft();
         rightClimbFromWallToSurface();
-        //leftClimbFromWallToSurface();
-        //doNotBuggingOnRightWallWhenAntGoingDown(movement);
-       // detachFromWall();   
     }
 
     private void antCanMove()
@@ -159,6 +119,7 @@ public class ant_movement : MonoBehaviour
     {
         if (isClimbing()&&!downAheadCheckPoint())
         {
+            antCantMove();
             animator.SetInteger("wallClimbSide", 2137);
         }      
     }
@@ -183,12 +144,10 @@ public class ant_movement : MonoBehaviour
     {
         if (PlayerAnt.gravityScale == 0)
         {
-            Debug.Log("is Climbing: TRUE");
             return true;
         }
         else
         {
-            Debug.Log("is Climbing: FALSE");
                 return false;
         }
     }
@@ -201,25 +160,24 @@ public class ant_movement : MonoBehaviour
 
     private void rotate_minus_90()
     {
-        m_FacingRight = !m_FacingRight;
         transform.Rotate(0f, 0f, -90f);
     }
 
-    private void doNotFallDownFromPlatform(float movement)
+    private void doNotFallDownFromPlatform()
     {
         if (!downAheadCheckPoint())
-            transform.position -= new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+            transform.position -= new Vector3(MOVEMENT, 0, 0) * Time.deltaTime * MovementSpeed;
     }
 
-    private void flippingWhenAntIsOnTheFloor(float movement)
+    private void flippingWhenAntIsOnTheFloor()
     {
         if (!isClimbing())
         {
-            if (movement > 0 && !m_FacingRight)
+            if (MOVEMENT > 0 && !m_FacingRight)
             {
                 Flip();
             }
-            else if (movement < 0 && m_FacingRight)
+            else if (MOVEMENT < 0 && m_FacingRight)
             {
                 Flip();
             }
@@ -316,11 +274,10 @@ public class ant_movement : MonoBehaviour
         }
     }
 
-    private void rightClimbOnWall()//**************************************************************************************************************************************
+    private void rightClimbOnWall()
     {
         if (rightCheck() && !isClimbing())
         {
-            //animator.SetInteger("wallClimbSide", 2323);
             transform.Rotate(0f, 0f, 90f);
             turnOffGravity();
         }
@@ -331,24 +288,7 @@ public class ant_movement : MonoBehaviour
         if ((isClimbing()))
         {
             turnOffGravity();
-            doNotClimbTooFarWhileUpToDownWallMoveRight();
             transform.position += new Vector3(0, MOVEMENT, 0) * Time.deltaTime * MovementSpeed;
-        }
-    }
-
-    private void doNotClimbTooFarWhileUpToDownWallMoveRight()
-    {
-        if (!downAheadCheckPoint() && Input.GetKey(KeyCode.W))
-        {
-            transform.position -= new Vector3(0, MOVEMENT, 0) * Time.deltaTime * MovementSpeed;
-        }
-    }
-
-    private void doNotClimbTooFarWhileUpToDownWallMoveLeft(float wallMovement)
-    {
-        if (topBehindCheckPoint() == false && Input.GetKey(KeyCode.W))
-        {
-            transform.position -= new Vector3(0, wallMovement, 0) * Time.deltaTime * MovementSpeed;
         }
     }
 
@@ -367,56 +307,5 @@ public class ant_movement : MonoBehaviour
         float x = transform.position.x;
         float y = transform.position.y;
         transform.position = new Vector2(x + 1.32f, y + 1f);
-        /*
-        Transform teleportationPosition = findDestinationTeleport();
-        PlayerAnt.transform.position = new Vector2(teleportationPosition.position.x, teleportationPosition.position.y);
-        */
     }
-
-    public Transform findDestinationTeleport()
-    {
-        for(int i=0;i<NUMBER_OF_TELEPORTS_PAIRS;i++)
-        {
-            Vector2 positionOfTeleport1 = teleports[i].getTeleport1().transform.position;
-            Vector2 positionOfTeleport2 = teleports[i].getTeleport2().transform.position;
-            Vector2 antPosition = PlayerAnt.transform.position;
-
-            if (Vector2.Distance(antPosition, positionOfTeleport1) <= distanceFromStartingTeleport)
-            {
-                Debug.Log("Start Teleport number: " + i);
-                return teleports[i].getTeleport2().transform;
-            }
-            if (Vector2.Distance(antPosition, positionOfTeleport2) <= distanceFromStartingTeleport)
-            {
-                Debug.Log("Start Teleport number: " + i);
-                return teleports[i].getTeleport1().transform;
-            }
-        }
-        return null;
-    }
-
-    public void setTeleportsInArray()
-    {
-        for(int i = 1; i <= NUMBER_OF_TELEPORTS_PAIRS; i++)
-        {
-            string teleport1Name = "TELEPORTER_" + i.ToString() + "_1";
-            string teleport2Name = "TELEPORTER_" + i.ToString() + "_2";
-            Transform teleport1 = GameObject.FindGameObjectWithTag(teleport1Name).GetComponent<Transform>();
-            Transform teleport2 = GameObject.FindGameObjectWithTag(teleport2Name).GetComponent<Transform>();
-            teleports.Add(new teleports_pair(teleport1, teleport2));
-        }
-    }
-    
-    public void detachFromWall()
-    {
-        if(isClimbing())
-        {
-            if((rightCheck())&&Input.GetKey(KeyCode.F))
-            {
-                turnOnGravity();
-                animator.SetInteger("wallClimbSide", 1234);
-            }
-        }
-    }
-
 }
