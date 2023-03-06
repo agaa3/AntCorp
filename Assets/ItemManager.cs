a1250ead5733147ae4862818b34598be7d29b6b0
Agata Granosik <agatagranosik3@gmail.com>
2023-03-06 13:02:16 +0100

    początkowa mapa, nowa mechanika zbierania małych mrówek (dawanie cukierka)

--- Assets/ItemManager.cs
+++ Assets/ItemManager.cs
@@ -55,9 +55,21 @@ public class ItemManager : MonoBehaviour
         }
     }
 
+	public int GetCandyNumber()
+    {
+        return candyNumber;        
+    }
+
     public void IncreaseCandyNumber()
     {
         candyNumber += 1;
+        numberOfCandiesImage.texture = allNumbers[candyNumber];
+        
+    }
+	
+	public void DecreaseCandyNumber()
+    {
+        candyNumber -= 1;
         numberOfCandiesImage.texture = allNumbers[candyNumber];
         
     }
