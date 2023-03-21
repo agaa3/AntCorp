1a48f13445342c273583a583e6c98e48e9ceca6e
Polybroo <viktor.chernikov@onet.pl>
2023-03-21 12:45:52 +0100

    Created enum and static class for layers and tags

---
+++ Assets/Scripts/AntCorp/Layer.cs
@@ -0,0 +1,14 @@
+namespace AntCorp
+{
+    public enum Layer 
+    {
+        Default,
+        TransparentFX,
+        IgnoreRaycast,
+        Reserved1,
+        Water,
+        UI,
+        Reserved2,
+        Reserved3
+    }
+}
---
+++ Assets/Scripts/AntCorp/Tag.cs
@@ -0,0 +1,12 @@
+namespace AntCorp
+{
+    public static class Tag
+    {
+        public const string Untagged = "Untagged";
+        public const string Respawn = "Respawn";
+        public const string Finish = "Finish";
+        public const string EditorOnly = "EditorOnly";
+        public const string MainCamera = "MainCamera";
+        public const string Player = "Player";
+    }
+}
