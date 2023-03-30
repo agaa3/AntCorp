97e3bf9d3a04cd3e5d5a01161b7f0896d084490c
KapitanMolotow <viktor.chernikov@onet.pl>
2023-03-30 18:05:57 +0200

    Item pickables now float on the ground

--- Assets/Scripts/ItemPickable.cs
+++ Assets/Scripts/ItemPickable.cs
@@ -4,4 +4,19 @@ public class ItemPickable : MonoBehaviour
 {
     public ItemType Type;
     public uint Amount;
+
+    private const float frequency = 4.25f;
+    private const float amplitude = 0.1f;
+    private const float offset = 0.1f;
+    private const bool applyDelay = true;
+
+
+    private Vector3 origin;
+
+    private void Awake(){
+        origin = transform.position;
+    }
+    private void Update(){
+        transform.position = origin + transform.up * offset + transform.up * amplitude * Mathf.Cos((applyDelay ? this.GetInstanceID() : 0) + Time.time * frequency);
+    }
 }
