a9a0b6e93b303ba24f8ece9d9f1bcc6297e17cfa
Polybroo <viktor.chernikov@onet.pl>
2023-04-03 20:06:49 +0200

    Water trigger now deals damage to the player

--- Assets/Scripts/WaterTrigger.cs
+++ Assets/Scripts/WaterTrigger.cs
@@ -5,6 +5,8 @@ public class WaterTrigger : MonoBehaviour
 {
     public Vector2 PushForce => new Vector2(0, 15);
     public const float PushSpeedLimit = -0.2f;
+    public float DamagePerTick = 25;
+    public float TickRate = 1f;
 
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -15,6 +17,32 @@ public class WaterTrigger : MonoBehaviour
             {
                 r.AddForce(PushForce);
             }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(Tag.Player))
+        {
+            InvokeRepeating(nameof(DamagePlayer), 0.0f, TickRate);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag(Tag.Player))
+        {
+            CancelInvoke(nameof(DamagePlayer));
+        }
+    }
+    private void DamagePlayer()
+    {
+        LivingMixin m = Player.Main.Mixin;
+        if (m.IsAlive)
+        {
+            m.DealDamage(DamagePerTick);
+        }
+        else
+        {
+            CancelInvoke(nameof(DamagePlayer));
         }
     }
 }
