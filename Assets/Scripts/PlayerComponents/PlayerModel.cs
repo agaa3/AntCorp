8cd1daf2cd2755de59571aceb631fa8bad48a4fe
Polybroo <viktor.chernikov@onet.pl>
2023-04-03 20:28:51 +0200

    Player model now reacts to taking damage and death

--- Assets/Scripts/PlayerComponents/PlayerModel.cs
+++ Assets/Scripts/PlayerComponents/PlayerModel.cs
@@ -7,21 +7,56 @@ using UnityEngine;
 public class PlayerModel : PlayerModule
 {
     public Animator Animator;
+    public SpriteRenderer Renderer;
     public PlayerController Controller => Player.Controller;
+
+    public Color DamageColor;
+    public Color DeadColor;
+
+    public bool ShownDamage;
+    public float ShowDamageTime = 0.25f;
 
 
     public override void OnInitialize(TimeState time)
     {
         Animator = GetComponent<Animator>();
-        Controller.PerformedTurn += TriggerTurn;
+        Renderer = GetComponent<SpriteRenderer>();
+
+        Controller.PerformedTurn += OnTurn;
+        Player.Mixin.OnTakeDamage += OnTakeDamage;
     }
     public override void OnUpdate(TimeState time)
     {
         Animator.SetBool("IsMoving", Controller.IsMoving);
+        if (!ShownDamage)
+        {
+            Renderer.color = Player.Mixin.IsAlive ? Color.white : DeadColor;
+        }
     }
 
-    private void TriggerTurn(bool isInside)
+    private void OnTakeDamage(float old, float nw)
+    {
+        if (!ShownDamage)
+        {
+            StartCoroutine(ShowDamage());
+        }
+    }
+    private void OnTurn(bool isInside)
     {
         Animator.SetTrigger(String.Format("BeginTurn{0}side", isInside ? "In" : "Out"));
+    }
+
+    private IEnumerator ShowDamage()
+    {
+        float t = 0.0f;
+        ShownDamage = true;
+        Renderer.color = DamageColor;
+        while (t < ShowDamageTime)
+        {
+            t += Time.deltaTime;
+            yield return null;
+        }
+        Renderer.color = Color.white;
+        ShownDamage = false;
     }
 }
