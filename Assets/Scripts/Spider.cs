567e4a5a076e33fa56d4b6455e4fe2c604cab8b2
Polybroo <viktor.chernikov@onet.pl>
2023-04-12 23:45:41 +0200

    Defined some fields and methods in Spider class, some prefab changes

--- Assets/Scripts/Spider.cs
+++ Assets/Scripts/Spider.cs
@@ -1,6 +1,45 @@
+using System;
 using UnityEngine;
 
+[DisallowMultipleComponent]
 public class Spider : MonoBehaviour
 {
+    [Header("State")]
+    public SpiderState State = SpiderState.Rest;
+    public float RestCooldown = 0f;
+    public float WebCurrentLength = 0f;
+    [Header("Settings")]
+    [Tooltip("Local position of rope's beginning.")]
+    public Vector2 WebStartPos = Vector2.zero;
+    [Tooltip("Maximum length of a web rope spider can weave.")]
+    public float WebMaxLength = 10f;
+    public float WebTriggerWidth = 0.35f;
+    [Tooltip("Speed of weaving a web rope in units per second.")]
+    public float WeaveSpeed = 0.65f;
+    [Tooltip("Speed of pulling a web rope by a spider in units per second.")]
+    public float PullSpeed = 0.85f;
+    public float RestDelay = 5f;
+    public bool HuntOnAwake = false;
+    [Header("Components")]
+    public Animator Animator;
+    public LineRenderer LineRenderer;
+    public BoxCollider2D Trigger;
 
+
+    private void UpdateTrigger()
+    {
+        throw new NotImplementedException();
+    }
+    private void UpdateLine()
+    {
+        throw new NotImplementedException();
+    }
+}
+public enum SpiderState
+{
+    Rest,
+    Weave,
+    Hunt,
+    Pull,
+    Digest
 }
